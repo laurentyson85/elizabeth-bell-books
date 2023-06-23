bd22b848d82abb5bc5ca9a57d40fd1439e949202
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-06-23 13:39:14 -0600

    Starter code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Elizabeth Bell Books
         </a>
       </header>
     </div>
