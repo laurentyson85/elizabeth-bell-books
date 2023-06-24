d5f5687b6c835eac91dcaa5fa839edb2b7a7091a
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-06-24 12:38:16 -0600

    Desktop author's note styling

--- src/components/AuthorNote.js
+++ src/components/AuthorNote.js
@@ -5,13 +5,14 @@ import authorNoteImg from "../images/author-note-image.jpeg";
 function AuthorNote() {
 
   return (   
-    <div className="bio">        
-      <img id="authorNoteImg" class="authorNoteImg" src={authorNoteImg} alt="Elizabeth Headshot"/>
-      <br></br>
-      <p>Hi! I'm Elizabeth Bell and thank you for visiting my website. The&nbsp;<i>Time for Dragons</i>&nbsp; children's book series was 100% inspired by my two pet leopard geckos, Geiko (the Little Yellow Dragon) and Dragon (the Little Pink Dragon - you can see her in the picture!)</p>
-      <p>I am so proud to have created a series that I hope children will love as much as I do for years to come and inspire other writers (and reptile-lovers) to do the same.</p>
-      <p>Feel free to contact me if you have any questions!</p>
-      <p>Elizabeth Bell</p>
+    <div className="authorNote">      
+        <img class="authorNoteImg" src={authorNoteImg} alt="Elizabeth Headshot"/>      
+      <div className="authorNoteChild-2">
+        <p>Hi! I'm Elizabeth Bell and thank you for visiting my website.The&nbsp;<i>Time for Dragons</i>&nbsp; <br></br> children's book series was 100% inspired by my two pet leopard geckos, <br></br>  Geiko (the Little Yellow Dragon) and Dragon (the Little Pink Dragon - you can see her  <br></br> in the picture!)</p>
+        <p>I am so proud to have created a series that I hope children will love as<br></br>  much as I do for years to come and inspire other writers (and reptile-lovers) to do the same.</p>
+        <p>Feel free to contact me if you have any questions!</p>
+        <p className="signature">Elizabeth Bell</p>
+      </div> 
     </div>  
   );
 }
