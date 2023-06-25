bdbcac654c5b4497df9ae7ae517f8c2834e1485d
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-06-25 14:37:44 -0600

    Mobile sizing for Little Dragons

--- src/components/LittleDragons.js
+++ src/components/LittleDragons.js
@@ -9,11 +9,9 @@ function LittleDragons() {
         <p className="littleDragonsTitle"><span style={{fontWeight: "bold"}}>The Little Dragons Are At It Again!</span></p>
         <p>The next book in the&nbsp;<i>Time for Dragons</i>&nbsp; series is here! This book features so much fun and colorful adventures from the Little Dragons that you will be begging for more. Get the next installment in the series today!</p>        
         <BuySequelButton />
-      </div>
-      <div className="littleDragonsImage">
+      </div>      
         <img class="playtimeCover" src={playtimeCover} alt="Playtime for Dragons Cover"/>
       </div>
-    </div>
   );
 }
 
