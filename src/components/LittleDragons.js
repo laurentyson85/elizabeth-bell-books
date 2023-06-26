28753477933ab237e7f81d5547a467b4b19ac7ab
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-06-26 12:20:35 -0600

    Little Dragons title edit

--- src/components/LittleDragons.js
+++ src/components/LittleDragons.js
@@ -6,7 +6,7 @@ function LittleDragons() {
   return (
     <div className="littleDragons">       
       <div className="littleDragonsTextButton">
-        <p className="littleDragonsTitle"><span style={{fontWeight: "bold"}}>The Little Dragons Are At It Again!</span></p>
+        <p className="littleDragonsTitle"><span style={{fontWeight: "bold"}}>The little dragons are at it again!</span></p>
         <p>The next book in the&nbsp;<i>Time for Dragons</i>&nbsp; series is here! This book features so much fun and colorful adventures from the Little Dragons that you will be begging for more. Get the next installment in the series today!</p>        
         <BuySequelButton />
       </div>      
