37bdd09e51ff9ab6cb760805ba36bf20a654355f
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-07-04 12:19:28 -0600

    Add gallery images

---
+++ src/components/Gallery/PreviousEvents.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PreviousEvents() {
+
+  return (   
+    <div className="gallery">        
+      Gallery
+    </div>  
+  );
+}
+
+export default PreviousEvents;
---
+++ src/components/Gallery/TimeGallery.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TimeGallery() {
+
+  return (   
+    <div className="gallery">        
+      Gallery
+    </div>  
+  );
+}
+
+export default TimeGallery;
