d677f7d7a73d7f3d804cb769eb4b38b98b4218e7
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-07-06 11:31:55 -0600

    Add basic contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const form = useRef();
 
-  return (   
-    <div className="contact">        
-      Contact
-    </div>  
+  // const success = <p>thank you for sending us a message</p>;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          alert(`message sent successfully... ${result.text}`);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div>
+      <h1>Contact Form</h1>
+      <form className='cf' ref={form} onSubmit={sendEmail}>
+        <div className='half left cf'>
+          <input type='text' placeholder='Name' name='user_name' />
+          <input type='email' placeholder='Email address' name='user_email' />
+        </div>
+        <div className='half right cf'>
+          <textarea name='message' type='text' placeholder='Message'></textarea>
+        </div>
+        <input type='submit' value='Submit' id='input-submit' />
+      </form>
+    </div>
   );
 }
 
