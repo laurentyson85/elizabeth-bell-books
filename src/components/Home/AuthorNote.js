10aaae83e00d14dda3b49607356ed9ace472ddad
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-07-06 10:51:57 -0600

    Change text link color and add link

--- src/components/Home/AuthorNote.js
+++ src/components/Home/AuthorNote.js
@@ -1,5 +1,6 @@
 import React from "react";
 import authorNoteImg from "../../images/author-note-image.jpeg";
+import { Link } from "react-router-dom";
 
 function AuthorNote() {
 
@@ -11,7 +12,7 @@ function AuthorNote() {
       <div className="authorNoteChild-2">
         <p>Hi! I'm Elizabeth Bell and thank you for visiting my website. The&nbsp;<i>Time for Dragons</i>&nbsp; children's book series was 100% inspired by my two pet leopard geckos, Geiko (the Little Yellow Dragon) and Dragon (the Little Pink Dragon - you can see her in the picture!)</p>
         <p>I am so proud to have created a series that I hope children will love as much as I do for years to come and inspire other writers (and reptile-lovers) to do the same.</p>
-        <p>Feel free to contact me if you have any questions!</p>
+        <p>Feel free to <Link className= "textLink" to="/contact">contact me</Link> if you have any questions!</p>
         <p className="signature">Elizabeth Bell</p>
       </div> 
     </div>  
