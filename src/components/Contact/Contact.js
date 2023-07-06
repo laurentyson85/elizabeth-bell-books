a476accfe8cdcf6ac22debeb993be077a8ba6991
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-07-06 16:47:10 -0600

    Add logic for the success message to disappear

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,50 +1,68 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 function Contact() {
-  const form = useRef();
 
-  //to do
-  //make form feilds required
-  //add success message and clear form
-  //utilize state to clear and set form
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
 
-  // const success = <p>thank you for sending us a message</p>;
+  const [sent, setSent] = useState(false);
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  function handleForm(event){
+    setFormData({
+      ...formData, [event.target.name]:event.target.value
+    })
+  }
+ 
+  function handleSubmit(event){
+    event.preventDefault()
 
     emailjs
-      .sendForm(
+      .send(
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
-        form.current,
+        {
+          user_name: formData.name,
+          user_email: formData.email,
+          message: formData.message,
+        },
         process.env.REACT_APP_PUBLIC_KEY
       )
       .then(
-        (result) => {
-          alert(`message sent successfully... ${result.text}`);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        })
+      )
+      .then(setSent(true))
+      .then(
+          setTimeout(() => {
+            setSent(false)
+          }, 5000)
+        )
+      };
+
+    
 
   return (
     <div className="contact">
       <p className="contactTitle">Contact Me</p>
       <p className="contactText">To place an order for an autographed book, a bookmark, to hire me for an event, or to ask me any questions, just use this form!</p>
-      <form className="cf" ref={form} onSubmit={sendEmail}>
+      <form className="cf" onSubmit={handleSubmit}>
         <div className="half left cf">
-          <input className="fields" required type="text" placeholder="Name" name="user_name" />
-          <input className="fields" required type="email" placeholder="Email address" name="user_email" />
+          <input className="fields" required type="text" name="name" placeholder="Name" value={formData.name} onChange={handleForm}/>
+          <input className="fields" required type="email" name="email" placeholder="Email address" value={formData.email} onChange={handleForm} />
         </div>
         <div className="half right cf">
-          <textarea name="message" required type="text" placeholder="Message..."></textarea>
+          <textarea required type="text" placeholder="Message..." name="message" value={formData.message} onChange={handleForm}></textarea>
         </div>
         <input className="submit" type="submit" value="Submit" id="input-submit" />
       </form>
+      { sent ? <p className="successMessage">Message sent! 🎉 I'll be in touch with you soon.</p> : null }      
     </div>
   );
 }
