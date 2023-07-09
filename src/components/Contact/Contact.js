d3a85bcd625bb8e9874ea7959ec179df65cdd2a4
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-07-09 15:01:36 -0600

    Remove useEffect

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
 import timeForDragons from "../../images/time-for-dragons.png";
 import ReCAPTCHA from "react-google-recaptcha";
